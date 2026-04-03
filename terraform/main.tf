f6c538de2646d79457c84f5987c1659e82a89563
nipamrohit <nipamrohit121@gmail.com>
2026-04-04 01:26:54 +0530

    terraform & ansible

---
+++ terraform/main.tf
@@ -0,0 +1,96 @@
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "devsecops-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# Route to Internet
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Associate Route Table
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Security Group (IMPORTANT for Ansible)
+resource "aws_security_group" "devsecops_sg" {
+  name   = "devsecops-sg"
+  vpc_id = aws_vpc.main_vpc.id
+
+  # SSH (for Ansible)
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # later restrict to your IP
+  }
+
+  # HTTP (for app/Nginx)
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Optional HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "devsecops_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.devsecops_sg.id]
+
+  tags = {
+    Name = "DevSecOps-EC2"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0f5ee92e2d63afc18"  # Ubuntu 22.04 (ap-south-1)
+}
+
+variable "key_name" {
+  default = "devsecops-key"
+}
